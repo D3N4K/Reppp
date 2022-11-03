147516ac77e0f85d40aec43f5c988ab05567a372
user03 <gumbatoff.emil@yandex.ru>
2022-11-03 10:44:40 +0300

    step

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿void Step()
+{
+    int step, a, b;
+    Console.WriteLine("1-e");
+    a = int.Parse(Console.ReadLine());
+    Console.WriteLine("2-e");
+    b = int.Parse(Console.ReadLine());
+    step = (int)Math.Pow(a,b);
+    Console.WriteLine(step);
+}
