c2d9f1f5c3ae64dac6f5cb6d20d87cbcd3bc8931
user03 <gumbatoff.emil@yandex.ru>
2022-11-03 10:42:57 +0300

    del

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿void Del()
+{
+    int del, a, b;
+    Console.WriteLine("1-e");
+    a = int.Parse(Console.ReadLine());
+    Console.WriteLine("2-e");
+    b = int.Parse(Console.ReadLine());
+    del = a / b;
+    Console.WriteLine(del);
+}
