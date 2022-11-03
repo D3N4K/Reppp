599a4ad27ee82a65e8b6a129694cdc95e9b8b2cf
user03 <gumbatoff.emil@yandex.ru>
2022-11-03 10:40:59 +0300

    sum

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿void Sum()
+{
+    int sum, a, b;
+    Console.WriteLine("1-e");
+    a = int.Parse(Console.ReadLine());
+    Console.WriteLine("2-e");
+    b = int.Parse(Console.ReadLine());
+    sum = a + b;
+    Console.WriteLine(sum);
+}
