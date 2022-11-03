97c2c0a70d03a7128bee66b915e735ce4f7c5336
user03 <gumbatoff.emil@yandex.ru>
2022-11-03 10:42:01 +0300

    raz

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿void Raz()
+{
+    int raz, a, b;
+    Console.WriteLine("1-e");
+    a = int.Parse(Console.ReadLine());
+    Console.WriteLine("2-e");
+    b = int.Parse(Console.ReadLine());
+    raz = a - b;
+    Console.WriteLine(raz);
+}
